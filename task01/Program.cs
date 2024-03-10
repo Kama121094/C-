7ab41e73fd57d6fcd80745c2d706f3cc1e0fb175
Alexey <varlord12101994@gmail.com>
2024-03-10 17:49:47 +0200

    added_new_vareibles lessontask01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world"); 
+﻿//Console.WriteLine("Hello world"); 
+
+//int value = 100;
+// Console.WriteLine(value);
+
+double a = 4.45;
+char c = '!';
+bool b = true;
+Console.WriteLine(a);
+Console.WriteLine(c);
+Console.WriteLine(b);
